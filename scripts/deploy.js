9fb8ec8cff9ba91c1ded8b55949834abfee06054
kiostech_ben <admin@kios.tech>
2021-05-03 16:28:50 +0800

    1. Developing the ddeployment mechanism

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -13,25 +13,35 @@ async function main() {
   // manually to make sure everything is compiled
   // await hre.run('compile');
 
-  // We get the contract to deploy
-  // const Greeter = await hre.ethers.getContractFactory("Greeter");
-  // const greeter = await Greeter.deploy("Hello, Hardhat!");
-  // await greeter.deployed();
-  // console.log("Greeter deployed to:", greeter.address);
+  // Company Registration Contract Deployment (ERC721)
+  const BusinessRegistrationContract = await hre.ethers.getContractFactory("CompanyRegistration");
+  const BusinessRegistratioDeployment = await BusinessRegistrationContract.deploy();
+  const BusinessRegistration = await BusinessRegistratioDeployment.deployed();
+  console.log("Business Registration is Deployed to : ",BusinessRegistration.address);
 
-  // Basic ERC20 Token Contract
-  // const ERC20Contract = await hre.ethers.getContractFactory("MyToken");
-  // const ERC20ContractDeployment = await ERC20Contract.deploy();
-  // await ERC20ContractDeployment.deployed();
-  // console.log("ERC20 Contract is Deployed to : ",ERC20ContractDeployment.address);
+  // Company Secretary (ERC721) Factory Deployment
+  const CompanySecretaryContract = await hre.ethers.getContractFactory("CompanySecretary721FactoryCloneV1");
+  const CompanySecretaryContractFactoryDeployment = await CompanySecretaryContract.deploy();
+  const CompanySecretaryFactory = await CompanySecretaryContractFactoryDeployment.deployed();
+  console.log("Company Secretary Factory Contract is Deployed to : ",CompanySecretaryFactory.address);
 
-  // ERC20 Token Factory
-  const ERC20ContractFactory = await hre.ethers.getContractFactory("FactoryClone");
-  const ERC20ContractFactoryDeployment = await ERC20ContractFactory.deploy();
-  await ERC20ContractFactoryDeployment.deployed();
-  console.log("ERC20 Factory Contract is Deployed to : ",ERC20ContractFactoryDeployment.address);
-  // 0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0
-  // 0x61ef99673A65BeE0512b8d1eB1aA656866D24296
+  // Company Shares Issuer Factory Deployment
+  const SharesIssuerContractFactory = await hre.ethers.getContractFactory("SharesIssuerFactoryCloneV1");
+  const SharesIssuerContractFactoryDeployment = await SharesIssuerContractFactory.deploy();
+  const SharesIssuerFactory = await SharesIssuerContractFactoryDeployment.deployed();
+  console.log("Shares Issuer Factory Contract is Deployed to : ",SharesIssuerFactory.address);
+
+  // Company Board Factory Deployment
+  const CompanyBoardContractFactory = await hre.ethers.getContractFactory("CompanyBoardFactoryCloneV1");
+  const CompanyBoardContractFactoryDeployment = await CompanyBoardContractFactory.deploy();
+  const CompanyBoardFactory = await CompanyBoardContractFactoryDeployment.deployed();
+  console.log("Company Board Factory Contract is Deployed to : ",CompanyBoardFactory.address);
+
+  // Business Registration is Deployed to :  0x8E1558B6B8a3ef346d187EABbe9B08C4DdFf336B
+  // Company Secretary Factory Contract is Deployed to :  0xA32a509e5f04d43438744a44e33Fb02FC55073ea
+  // Shares Issuer Factory Contract is Deployed to :  0x87943Ac856a51d99e3e039Ff1eF976D316f06118
+  // Company Board Factory Contract is Deployed to :  0xEaE3e0Be728155C099C3b940462e50c6600E307F
+
 }
 
 // We recommend this pattern to be able to use async/await everywhere
